81505c739d89c4510d64dbd1f757ad4c170d69f4
Marques Avery <averymarques4@gmail.com>
2020-08-14 22:02:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,18 @@
 class Dog
+  def name=(new_name)
+    @dog_name = new_name
+  end
+
+  def name
+    @dog_name
+  end
+
+  def breed=(new_breed)
+    @dog_breed = new_breed
+  end
+
+  def breed
+    @dog_breed
+  end
+
 end
