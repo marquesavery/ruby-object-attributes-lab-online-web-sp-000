9a3d1c1be0360b9c87bb21ae0fac70f903fe8dcd
Marques Avery <averymarques4@gmail.com>
2020-08-14 22:05:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
-  def initialize(new_name, new_breed)
-    @dog_name = new_name
-    @dog_breed = new_breed
-  end
-  
+#  def initialize(new_name, new_breed)
+#    @dog_name = new_name
+#    @dog_breed = new_breed
+#  end
+
   def name=(new_name)
     @dog_name = new_name
   end
